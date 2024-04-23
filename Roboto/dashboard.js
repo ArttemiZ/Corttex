60154177dafd04d2e8175cc7181fbeeadb54b016
ArttemiZ <148831708+ArttemiZ@users.noreply.github.com>
2024-04-22 21:48:38 -0300

    Add files via upload

---
+++ Roboto/dashboard.js
@@ -0,0 +1,47 @@
+/* globals Chart:false */
+
+(() => {
+  'use strict'
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        '15-20',
+        '21-30',
+        '31-40',
+        '41-50',
+        '50+'
+      ],
+      datasets: [{
+        data: [
+          18,
+          29,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          boxPadding: 3
+        }
+      }
+    }
+  })
+})()
